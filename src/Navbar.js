7284aa3016e9501493fc03c244120951551d2599
nrdnisml <nurudinismail69@gmail.com>
2021-03-15 15:51:14 +0700

    w5

---
+++ src/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Navbar extends React.Component{
+    render(){
+        return(
+            <div>
+                <p>Navbar</p>
+            </div>
+        )
+    }
+}
+export default Navbar;
